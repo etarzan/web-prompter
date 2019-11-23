c6259d3019f144e1f43462c25e1d528123d18703
etarzan <sharma.rajarshi@gmail.com>
2019-11-23 18:35:00 +0530

    recitified static value for view child

--- src/app/entry-component/entry-unit/entry-unit.component.ts
+++ src/app/entry-component/entry-unit/entry-unit.component.ts
@@ -2,62 +2,63 @@ import { Component, OnInit, ViewChild, TemplateRef, ElementRef } from '@angular/
 import { PrompterService } from 'src/app/prompter.service';
 
 @Component({
-  selector: 'app-entry-unit',
-  templateUrl: './entry-unit.component.html',
-  styleUrls: ['./entry-unit.component.css']
+    selector: 'app-entry-unit',
+    templateUrl: './entry-unit.component.html',
+    styleUrls: ['./entry-unit.component.css']
 })
 export class EntryUnitComponent implements OnInit {
 
-  lines = [];
-  lineinput = '';
-  editIndex: number;
-  @ViewChild('input_text', {static:false}) textInputElement: ElementRef<HTMLInputElement>;
+    lines = [];
+    lineinput = '';
+    editIndex: number;
 
-  constructor(private readonly prompterService: PrompterService) { }
-  ngOnInit() {
-    this.editIndex = null;
-    this.prompterService.getLines().subscribe(data => {
-      this.lines = data;
-    });
-    this.textInputElement.nativeElement.autofocus = true;
-  }
+    @ViewChild('input_text', { static: true }) textInputElement: ElementRef<HTMLInputElement>;
 
-  addInputLineOnEnter(event) {
-    if (event.keyCode === 13) {
-      if (!(this.editIndex === undefined || this.editIndex === null) && this.editIndex > -1) {
-        this.prompterService.editLine(this.lineinput, this.editIndex);
+    constructor(private readonly prompterService: PrompterService) { }
+    ngOnInit() {
         this.editIndex = null;
-      } else {
-        this.prompterService.addLine(this.lineinput);
-      }
-      this.lineinput = '';
+        this.prompterService.getLines().subscribe(data => {
+            this.lines = data;
+        });
+        this.textInputElement.nativeElement.autofocus = true;
     }
 
-  }
+    addInputLineOnEnter(event) {
+        if (event.keyCode === 13) {
+            if (!(this.editIndex === undefined || this.editIndex === null) && this.editIndex > -1) {
+                this.prompterService.editLine(this.lineinput, this.editIndex);
+                this.editIndex = null;
+            } else {
+                this.prompterService.addLine(this.lineinput);
+            }
+            this.lineinput = '';
+        }
 
-  addInputLine() {
-    if (!(this.editIndex === undefined || this.editIndex === null) && this.editIndex > -1) {
-      this.prompterService.editLine(this.lineinput, this.editIndex);
-      this.editIndex = null;
-    } else {
-      this.prompterService.addLine(this.lineinput);
     }
-    this.lineinput = '';
-  }
 
-  deleteElement(index) {
-    if (!(this.lineinput.trim().length > 1 || this.editIndex !== null)) {
-      this.prompterService.deleteElement(index);
-    } else {
-      alert('Can\'t Delete while editing list');
+    addInputLine() {
+        if (!(this.editIndex === undefined || this.editIndex === null) && this.editIndex > -1) {
+            this.prompterService.editLine(this.lineinput, this.editIndex);
+            this.editIndex = null;
+        } else {
+            this.prompterService.addLine(this.lineinput);
+        }
+        this.lineinput = '';
     }
-   // this.textInputElement.nativeElement.focus();
-  }
 
-  onClickEdit(index: number) {
-    this.lineinput = this.lines[index];
-    this.editIndex = index;
-    this.textInputElement.nativeElement.focus();
-  }
+    deleteElement(index) {
+        if (!(this.lineinput.trim().length > 1 || this.editIndex !== null)) {
+            this.prompterService.deleteElement(index);
+        } else {
+            alert('Can\'t Delete while editing list');
+        }
+        // this.textInputElement.nativeElement.focus();
+    }
+
+    onClickEdit(index: number) {
+        this.lineinput = this.lines[index];
+        this.editIndex = index;
+        this.textInputElement.nativeElement.focus();
+    }
 
 }
