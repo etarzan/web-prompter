44f458386fbea6b231b6a4dbde871a1345200041
etarzan <sharma.rajarshi@gmail.com>
2019-11-25 23:49:18 +0530

    added controlunit control ui elements

--- src/app/entry-component/control-unit/control-unit.component.ts
+++ src/app/entry-component/control-unit/control-unit.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { MatButtonToggleGroup, MatButtonToggle } from '@angular/material';
 
 @Component({
-  selector: 'app-control-unit',
-  templateUrl: './control-unit.component.html',
-  styleUrls: ['./control-unit.component.css']
+    selector: "app-control-unit",
+    templateUrl: "./control-unit.component.html",
+    styleUrls: ["./control-unit.component.css"]
 })
 export class ControlUnitComponent implements OnInit {
+    textSize = "Small";
 
-  constructor() { }
+    @ViewChild('backColorGroup', { static: true }) backColorGroup: ElementRef<MatButtonToggleGroup>;
+    @ViewChild('textColorGroup', { static: true }) textColorGroup: MatButtonToggleGroup;
 
-  ngOnInit() {
-  }
+    textColorChecked: boolean = false;
+    backColorChecked: boolean = false;
 
+    constructor() { }
+
+    ngOnInit() { }
+
+
+    checkTextColor(event: any) {
+        console.log(this.textColorGroup.value);
+    }
+
+    checkBackColor(event: any) {
+        console.log(event);
+    }
 }
