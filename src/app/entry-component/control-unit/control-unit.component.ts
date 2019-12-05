95d1e2a119ff6f8b04854c0ba67963125c6e9632
etarzan <sharma.rajarshi@gmail.com>
2019-12-06 02:43:08 +0530

    added change event and view child for control unit

--- src/app/entry-component/control-unit/control-unit.component.ts
+++ src/app/entry-component/control-unit/control-unit.component.ts
@@ -1,30 +1,42 @@
-import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
-import { MatButtonToggleGroup, MatButtonToggle } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
+import { MatButtonToggleGroup, MatSlider } from '@angular/material';
+import { PrompterService } from 'src/app/prompter.service';
 
 @Component({
     selector: "app-control-unit",
     templateUrl: "./control-unit.component.html",
     styleUrls: ["./control-unit.component.css"]
 })
-export class ControlUnitComponent implements OnInit {
+export class ControlUnitComponent implements OnInit, AfterViewInit {
+
+
     textSize = "Small";
 
-    @ViewChild('backColorGroup', { static: true }) backColorGroup: ElementRef<MatButtonToggleGroup>;
+    @ViewChild('backColorGroup', { static: true }) backColorGroup: MatButtonToggleGroup;
     @ViewChild('textColorGroup', { static: true }) textColorGroup: MatButtonToggleGroup;
+    @ViewChild('fontsize', { static: true }) fontsize: MatSlider;
+    @ViewChild('scrollspeed', { static: true }) scrollspeed: MatButtonToggleGroup;
 
     textColorChecked: boolean = false;
     backColorChecked: boolean = false;
 
-    constructor() { }
+    constructor(private readonly prompterSvc: PrompterService) { }
 
     ngOnInit() { }
 
-
-    checkTextColor(event: any) {
-        console.log(this.textColorGroup.value);
+    ngAfterViewInit(): void {
+        this.prompterSvc.inputData.fontSize = this.fontsize.value;
+        this.prompterSvc.inputData.scrollSpeed = this.scrollspeed.value;
+        this.prompterSvc.inputData.textColor = this.textColorGroup.value;
+        this.prompterSvc.inputData.backColor = this.backColorGroup.value;
     }
 
-    checkBackColor(event: any) {
-        console.log(event);
+
+    onChange(event: any) {
+        this.prompterSvc.inputData.fontSize = this.fontsize.value;
+        this.prompterSvc.inputData.scrollSpeed = this.scrollspeed.value;
+        this.prompterSvc.inputData.textColor = this.textColorGroup.value;
+        this.prompterSvc.inputData.backColor = this.backColorGroup.value;
     }
+
 }
