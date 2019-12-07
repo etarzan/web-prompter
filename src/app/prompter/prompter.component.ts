cb7bf25f2bb6074ec2ebb5eb28fecb84803644a2
etarzan <sharma.rajarshi@gmail.com>
2019-12-08 01:58:15 +0530

    refactor changes

--- src/app/prompter/prompter.component.ts
+++ src/app/prompter/prompter.component.ts
@@ -2,20 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { PrompterService } from '../prompter.service';
 
 @Component({
-  selector: 'app-prompter',
-  templateUrl: './prompter.component.html',
-  styleUrls: ['./prompter.component.css']
+    selector: 'app-prompter',
+    templateUrl: './prompter.component.html',
+    styleUrls: ['./prompter.component.css']
 })
 export class PrompterComponent implements OnInit {
 
-    prompterText: string [] = [''];
+    curSec: number = 0;
+    progressbarValue = 100;
+    prompterText: string[] = [''];
+    fontsize: Number;
+    scrollspeed: Number;
+    textcolor: string;
+    backcolor: string;
 
-  constructor(private prompterService: PrompterService) { }
+    constructor(private prompterService: PrompterService) { }
 
-  ngOnInit() {
-    this.prompterService.getLines().subscribe( lines =>{
-        this.prompterText = lines;
-    });
-  }
+    ngOnInit() {
+        this.prompterService.getData().subscribe(inputData => {
+            this.prompterText = inputData.inputLines;
+            this.fontsize = inputData.fontSize;
+            this.scrollspeed = inputData.scrollSpeed;
+            this.textcolor = inputData.textColor;
+            this.backcolor = inputData.backColor;
+        });
+    }
 
 }
